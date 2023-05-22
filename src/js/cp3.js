a1e43e0ec0d2741f113dc7bf6c48a942af0752e4
PhabloFiap <rm550687@fiap.com.br>
2023-05-22 20:21:34 -0300

    modoescuro arrumado

---
+++ src/js/cp3.js
@@ -0,0 +1,78 @@
+//Formulario
+
+document.addEventListener("DOMContentLoaded", function() {
+    const formulario = document.getElementById("meuFormulario");
+
+    formulario.addEventListener("submit", function(event) {
+      event.preventDefault();
+
+      const inputPrimeiroNome = document.getElementById("primeiroNome");
+      const inputSegundoNome = document.getElementById("segundoNome");
+      const inputEmail = document.getElementById("email");
+      const inputSenha = document.getElementById("senha");
+      const inputConfirmacaoSenha = document.getElementById("confirmacaoSenha");
+
+      resetarValidacao();
+
+      if (!inputPrimeiroNome.value.trim()) {
+        marcarInvalido(inputPrimeiroNome);
+      }
+
+      if (!inputSegundoNome.value.trim()) {
+        marcarInvalido(inputSegundoNome);
+      }
+
+      if (!inputEmail.value.trim() || !emailEValido(inputEmail.value)) {
+        marcarInvalido(inputEmail);
+      }
+
+      if (inputSenha.value.length < 6) {
+        marcarInvalido(inputSenha);
+      }
+
+      if (inputConfirmacaoSenha.value !== inputSenha.value) {
+        marcarInvalido(inputConfirmacaoSenha);
+      }
+
+      if (formulario.querySelectorAll(".error").length === 0) {
+        formulario.submit();
+      } else {
+        alert("Preencha corretamente todos os campos do formulario.");
+      }
+    });
+
+    function marcarInvalido(elementoInput) {
+      elementoInput.classList.add("error");
+    }
+
+    function resetarValidacao() {
+      const inputs = formulario.querySelectorAll("input");
+      inputs.forEach(function(input) {
+        input.classList.remove("error");
+      });
+    }
+
+    function emailEValido(email) {
+      const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regexEmail.test(email);
+    }
+  });
+
+
+
+  const botaoModoEscuro = document.getElementById("botaoEscuro");
+
+  botaoModoEscuro.addEventListener("click", function() {
+    const corpo = document.body;
+    corpo.classList.toggle("modoEscuro");
+    
+    const modoAtual = corpo.classList.contains("modoEscuro") ? "modoEscuro" : "modoClaro";
+  
+    botaoModoEscuro.textContent = modoAtual === "modoEscuro" ? "Modo Claro" : "Modo Escuro";
+    
+    const elementosModoEscuro = document.querySelectorAll(".alteraCor");
+  
+    elementosModoEscuro.forEach(function(elemento) {
+      elemento.classList.toggle("modoEscuro");
+    });
+  });
